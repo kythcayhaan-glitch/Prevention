0256f6d1291848ba5a5acb84e77186deae4e7296
kythcayhaan-glitch <kythcayhaan@gmail.com>
2026-04-29 09:56:54 +0200

    Remove visite management module (moved to mdp project)
    All visite-related controllers, entities, repositories, forms and templates
    have been removed. Dashboard updated to show only RQTH, FIPHFP and interventions.

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,7 +6,6 @@ use App\Repository\DocumentRepository;
 use App\Repository\FiphfpRepository;
 use App\Repository\InterRepository;
 use App\Repository\RqthRepository;
-use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,14 +14,12 @@ class DashboardController extends AbstractController
 {
     #[Route('/', name: 'app_dashboard')]
     public function index(
-        VisiteRepository $visiteRepo,
         RqthRepository $rqthRepo,
         FiphfpRepository $fiphfpRepo,
         InterRepository $interRepo,
         DocumentRepository $docRepo,
     ): Response {
         return $this->render('dashboard/index.html.twig', [
-            'nb_visites' => $visiteRepo->count([]),
             'nb_rqth'    => $rqthRepo->count([]),
             'nb_fiphfp'  => $fiphfpRepo->count([]),
             'nb_inter'   => $interRepo->count([]),
